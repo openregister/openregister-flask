95b2eae8d47eb84d75db88be0da5d0aa85bf75ef
psd <psd@whatfettle.com>
2015-01-25 22:18:31 +0000

    Base tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,10 +2,22 @@ import application
 
 
 app = application.app.test_client()
-base_url = 'http://education.thingstance.org/'
+registry_url = 'http://thingstance.org/'
+education_url = 'http://education.thingstance.org/'
 
 
-def test_get_missing_thing_404():
+def test_get_unknown_domain_404():
+    response = app.get('/', base_url='http://no-such-domain.example.org')
+    assert response.status_code == 404
+
+
+def test_get_tag_404():
+    tag = "ThisDoesNotExist"
+    response = app.get('/%s' % tag, base_url=education_url)
+    assert response.status_code == 404
+
+
+def test_get_thing_404():
     hash = "aaaaaaaaa"
-    response = app.get('/Thing/%s' % hash, base_url=base_url)
+    response = app.get('/Thing/%s' % hash, base_url=education_url)
     assert response.status_code == 404
