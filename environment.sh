13024d8c72fb1baab114bc87da42c16e739e59d0
Adam Shimali <adam.shimali@gmail.com>
2015-03-17 15:11:02 +0000

    revert environment file used in travis build

---
+++ environment.sh
@@ -0,0 +1,9 @@
+export SETTINGS='config.DevelopmentConfig'
+export REGISTER_DOMAIN='register.dev'
+export BASE_URL='http://'$REGISTER_DOMAIN
+export MONGO_URI='mongodb://127.0.0.1:27017/thingstance'
+export PAGE_SIZE=50
+export PORT=8000
+export SECRET_KEY='not-secret'
+export HEROKU_KEY=`heroku auth:token`
+export GITHUB_ORG='https://github.com/openregister'
