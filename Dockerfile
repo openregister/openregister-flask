1b4c87b8e88951d4ed9cab8161042fa443bf172e
Adam Shimali <adam.shimali@gmail.com>
2015-03-24 13:58:59 +0000

    final part of dockerisation

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,11 @@
-FROM python:3.4-onbuild
+FROM python:3.4.2
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY requirements/* /usr/src/app/requirements/
+RUN pip install -r requirements/dev.txt
+
+COPY . /usr/src/app
+
 CMD ["gunicorn", "--config=gunicorn.py", "application.views:app"]
--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,15 @@
 import os
 import logging
+import time
 
 from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(os.environ.get('SETTINGS'))
+
+if not app.debug:
+    app.logger.addHandler(logging.StreamHandler())
+    app.logger.setLevel(logging.INFO)
 
 
 # we'll use this handle to db to check collections
@@ -13,8 +18,5 @@ import pymongo
 client = pymongo.MongoClient(app.config['MONGO_URI'])
 db = client.get_default_database()
 
-from application.views import *  # NOQA
 
-if not app.debug:
-    app.logger.addHandler(logging.StreamHandler())
-    app.logger.setLevel(logging.INFO)
+from application.views import *  # NOQA
